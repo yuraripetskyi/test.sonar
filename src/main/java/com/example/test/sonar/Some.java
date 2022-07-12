e1f1aada1b0ad72ffdb490b3c9e21cfaea13c31a
Yurii <yuraripeckiy@gmail.com>
2022-07-12 21:09:51 +0300

    code smell test

---
+++ src/main/java/com/example/test/sonar/Some.java
@@ -0,0 +1,15 @@
+package com.example.test.sonar;
+
+public class Some {
+    public void smell() {
+        while (true) {
+            System.out.println(123);
+            System.out.println(123);
+            System.out.println(123);
+            System.out.println(123);
+            System.out.println(123);
+            System.out.println(123);
+            System.out.println(123);
+        }
+    }
+}
