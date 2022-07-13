4604d384b79aec24c18499b7c0554b2ebeb73c67
Yurii <yuraripeckiy@gmail.com>
2022-07-13 12:22:05 +0300

    code smell test

--- src/main/java/com/example/test/sonar/Some.java
+++ src/main/java/com/example/test/sonar/Some.java
@@ -1,5 +1,7 @@
 package com.example.test.sonar;
 
+import java.math.BigDecimal;
+
 public class Some {
     public void smell() {
         while (true) {
@@ -11,5 +13,17 @@ public class Some {
             System.out.println(123);
             System.out.println(123);
         }
+    }
+
+
+    private static int test() {
+       try {
+           while (true) {
+               BigDecimal bigDecimal = new BigDecimal(1);
+           }
+       } catch (Throwable throwable) {
+           System.out.println("123");
+       }
+       return 1;
     }
 }
